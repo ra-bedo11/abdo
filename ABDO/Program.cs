7d18a5494f5c047fcd4888d6340d0c315d39197e
yoyo <yoyo@PC1>
2017-06-24 05:30:25 +0300

    Add project files.

---
+++ ABDO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///https://github.com/ra-bedo11/C-Codes_1.git
+            ///
+            Console.WriteLine("Abdallah hassan shamah");
+        }
+    }
+}
